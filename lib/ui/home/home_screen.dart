3c376e24c8f7fbdd3379e5bc8510489dd71145ed
Marko <markodj10@gmail.com>
2025-10-29 22:12:33 +0100

    Added Top Rated,Popular and Trending Categories.

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:framed_v2/ui/home/home_screen_image.dart';
+import 'package:framed_v2/ui/home/horiz_movies.dart';
+import 'package:framed_v2/ui/home/title_row.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,26 +15,34 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          color: const Color(0xFF111111),
-          child: Column(
-            children: [
-              const Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 16.0, 0, 24),
-                  child: Text(
-                    'Now Playing',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            color: const Color(0xFF111111),
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 16.0, 0, 24),
+                    child: Text(
+                      'Now Playing',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              HomeScreenImage(),
-            ],
+                HomeScreenImage(),
+                TitleRow(text: 'Trending', onMoreClicked: () {}),
+                const HorizontalMovies(movies: images),
+                TitleRow(text: 'Popular', onMoreClicked: () {}),
+                const HorizontalMovies(movies: images),
+                TitleRow(text: 'Top Rated', onMoreClicked: () {}),
+                const HorizontalMovies(movies: images),
+              ],
+            ),
           ),
         ),
       ),
--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -7,12 +7,38 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-var index = 0;
-final List<Widget> screens = <Widget>[]; //tu stigli
-
 class _MainScreenState extends State<MainScreen> {
+  var index = 0;
+  final List<Widget> screens = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    screens.add(HomeScreen());
+    screens.add(const Placeholder());
+    screens.add(const Placeholder());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.album), label: 'Genre'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+        ],
+        currentIndex: index,
+        onTap: (navIndex) {
+          setState(() {
+            index = navIndex;
+          });
+        },
+      ),
+    );
   }
 }
